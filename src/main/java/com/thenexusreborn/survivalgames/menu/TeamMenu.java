d30a50e1e052bcf616c113be8677c754342d95ad
Firestar311 <slaterandrew0398@gmail.com>
2022-06-11 18:44:24 -0500

    Fixed a bug where vanished staff members can be seen in spectator gui menu

--- src/main/java/com/thenexusreborn/survivalgames/menu/TeamMenu.java
+++ src/main/java/com/thenexusreborn/survivalgames/menu/TeamMenu.java
@@ -14,16 +14,18 @@ public class TeamMenu extends Menu {
         
         if (plugin.getGame() != null) {
             for (GamePlayer player : plugin.getGame().getPlayers().values()) {
-                if (player.getTeam() == team) {
-                    ItemStack skull = SpigotUtils.getPlayerSkull(player.getNexusPlayer().getPlayer());
-                    ItemMeta meta = skull.getItemMeta();
-                    meta.setDisplayName(MCUtils.color(player.getNexusPlayer().getDisplayName()));
-                    skull.setItemMeta(meta);
-                    Button button = new Button(skull);
-                    button.setLeftClickAction((p, menu, clickType) -> {
-                        p.teleport(player.getNexusPlayer().getPlayer().getLocation()); //TODO This will be replaced with a menu at some point
-                    });
-                    addElement(button);
+                if (!player.getNexusPlayer().getPreferences().get("vanish").getValue()) {
+                    if (player.getTeam() == team) {
+                        ItemStack skull = SpigotUtils.getPlayerSkull(player.getNexusPlayer().getPlayer());
+                        ItemMeta meta = skull.getItemMeta();
+                        meta.setDisplayName(MCUtils.color(player.getNexusPlayer().getDisplayName()));
+                        skull.setItemMeta(meta);
+                        Button button = new Button(skull);
+                        button.setLeftClickAction((p, menu, clickType) -> {
+                            p.teleport(player.getNexusPlayer().getPlayer().getLocation()); //TODO This will be replaced with a menu at some point
+                        });
+                        addElement(button);
+                    }
                 }
             }
         }
