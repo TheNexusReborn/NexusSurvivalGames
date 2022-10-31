fb866e69b087c2ae06010654127b8c9a9a3da0eb
Firestar311 <slaterandrew0398@gmail.com>
2022-10-31 14:27:51 -0500

    Removed the Enderman Pearl Cooldown

--- src/main/java/com/thenexusreborn/survivalgames/mutations/impl/EndermanMutation.java
+++ src/main/java/com/thenexusreborn/survivalgames/mutations/impl/EndermanMutation.java
@@ -1,41 +1,11 @@
 package com.thenexusreborn.survivalgames.mutations.impl;
 
-import com.thenexusreborn.nexuscore.util.timer.Timer;
 import com.thenexusreborn.survivalgames.mutations.*;
-import com.thenexusreborn.survivalgames.mutations.timer.EpearlCooldownCallback;
 
 import java.util.UUID;
 
 public class EndermanMutation extends Mutation {
-    
-    private Timer epearlCooldownTimer;
-    
     protected EndermanMutation(UUID player, UUID target) {
         super(MutationType.ENDERMAN, player, target);
-    }
-    
-    public boolean isEpearlOnCooldown() {
-        if (epearlCooldownTimer == null) {
-            return false;
-        }
-        
-        return epearlCooldownTimer.getSecondsLeft() > 0;
-    }
-    
-    public void startEpearlCooldownTimer() {
-        epearlCooldownTimer = new Timer(new EpearlCooldownCallback(this));
-        epearlCooldownTimer.setLength(3000);
-        epearlCooldownTimer.run();
-    }
-    
-    public Timer getEpearlCooldownTimer() {
-        return epearlCooldownTimer;
-    }
-    
-    public void resetEpearlCooldown() {
-        if (epearlCooldownTimer != null) {
-            epearlCooldownTimer.cancel();
-        }
-        epearlCooldownTimer = null;
     }
 }
