501c803797f72400430336d74074e766f427c4b1
Firestar311 <slaterandrew0398@gmail.com>
2023-02-24 03:54:00 -0600

    Replaced the Registry class with the one from StarLib

--- src/main/java/com/thenexusreborn/survivalgames/settings/SettingRegistry.java
+++ src/main/java/com/thenexusreborn/survivalgames/settings/SettingRegistry.java
@@ -1,11 +1,11 @@
 package com.thenexusreborn.survivalgames.settings;
 
+import com.starmediadev.starlib.Registry;
 import com.thenexusreborn.api.frameworks.value.Value;
-import com.thenexusreborn.api.registry.Registry;
 import com.thenexusreborn.survivalgames.settings.object.Setting;
 import com.thenexusreborn.survivalgames.settings.object.Setting.Info;
 
-public class SettingRegistry extends Registry<Setting.Info> {
+public class SettingRegistry extends Registry<Info> {
     
     public void register(String name, String displayName, String description, String type, Value defaultValue) {
         register(new Info(name, displayName, description, type, defaultValue));
