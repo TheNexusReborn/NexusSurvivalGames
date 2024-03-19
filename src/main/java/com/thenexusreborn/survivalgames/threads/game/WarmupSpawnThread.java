42b73b7490849dd1dc3d9897864783fe9e0b4fdf
Firestar311 <slaterandrew0398@gmail.com>
2024-03-18 20:53:56 -0600

    Fixed a Streams bug with the WarmupSpawnThread

--- src/main/java/com/thenexusreborn/survivalgames/threads/game/WarmupSpawnThread.java
+++ src/main/java/com/thenexusreborn/survivalgames/threads/game/WarmupSpawnThread.java
@@ -1,6 +1,7 @@
 package com.thenexusreborn.survivalgames.threads.game;
 
 import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.thenexusreborn.gamemaps.model.MapSpawn;
 import com.thenexusreborn.gamemaps.model.SGMap;
 import com.thenexusreborn.nexuscore.api.NexusThread;
@@ -18,7 +19,7 @@ import java.util.stream.Stream;
 
 public class WarmupSpawnThread extends NexusThread<SurvivalGames> {
     
-    public static final Stream<GameState> stateStream = Stream.of(GameState.WARMUP, GameState.WARMUP_DONE, GameState.DEATHMATCH_WARMUP, GameState.DEATHMATCH_WARMUP_DONE, GameState.TELEPORT_START, GameState.TELEPORT_DEATHMATCH, GameState.TELEPORT_START_DONE, GameState.TELEPORT_DEATHMATCH_DONE);
+    public static final GameState[] states = new GameState[] {GameState.WARMUP, GameState.WARMUP_DONE, GameState.DEATHMATCH_WARMUP, GameState.DEATHMATCH_WARMUP_DONE, GameState.TELEPORT_START, GameState.TELEPORT_DEATHMATCH, GameState.TELEPORT_START_DONE, GameState.TELEPORT_DEATHMATCH_DONE};
     
     public WarmupSpawnThread(SurvivalGames plugin) {
         super(plugin, 1L, 1L, false);
@@ -33,29 +34,31 @@ public class WarmupSpawnThread extends NexusThread<SurvivalGames> {
         Game game = plugin.getGame();
         SGMap gameMap = game.getGameMap();
 
-        if (stateStream.noneMatch(gameState -> game.getState() == gameState)) {
+        if (Stream.of(states).noneMatch(gameState -> game.getState() == gameState)) {
             return;
         }
 
-        BiMap<UUID, Integer> spawns = game.getSpawns().inverse();
-        for (GamePlayer gamePlayer : game.getPlayers().values()) {
-            if (gamePlayer == null || gamePlayer.getTeam() != GameTeam.TRIBUTES) {
-                continue;
-            }
+        try {
+            BiMap<UUID, Integer> spawns = HashBiMap.create(game.getSpawns()).inverse();
+            for (GamePlayer gamePlayer : game.getPlayers().values()) {
+                if (gamePlayer == null || gamePlayer.getTeam() != GameTeam.TRIBUTES) {
+                    continue;
+                }
 
-            Player player = Bukkit.getPlayer(gamePlayer.getUniqueId());
-            Location playerLocation = player.getLocation();
-            MapSpawn spawn = gameMap.getSpawns().get(spawns.get(player.getUniqueId()));
-            if (spawn == null) {
-                continue;
+                Player player = Bukkit.getPlayer(gamePlayer.getUniqueId());
+                Location playerLocation = player.getLocation();
+                MapSpawn spawn = gameMap.getSpawns().get(spawns.get(player.getUniqueId()));
+                if (spawn == null) {
+                    continue;
+                }
+
+                Location spawnLocation = spawn.toGameLocation(gameMap.getWorld(), gameMap.getCenterLocation());
+                if (playerLocation.getBlockX() != spawnLocation.getBlockX() || playerLocation.getBlockZ() != spawnLocation.getBlockZ()) {
+                    spawnLocation.setYaw(playerLocation.getYaw());
+                    spawnLocation.setPitch(playerLocation.getPitch());
+                    player.teleport(spawnLocation);
+                }
             }
-            
-            Location spawnLocation = spawn.toGameLocation(gameMap.getWorld(), gameMap.getCenterLocation());
-            if (playerLocation.getBlockX() != spawnLocation.getBlockX() || playerLocation.getBlockZ() != spawnLocation.getBlockZ()) {
-                spawnLocation.setYaw(playerLocation.getYaw());
-                spawnLocation.setPitch(playerLocation.getPitch());
-                player.teleport(spawnLocation);
-            }
-        }
+        } catch (Exception e) {}
     }
 }
