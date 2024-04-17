3696d895449145dea71d634b182f040d2f1fdd86
Firestar311 <slaterandrew0398@gmail.com>
2024-04-16 18:35:26 -0600

    Fixed a spelling problem in the ControlType class

--- src/main/java/com/thenexusreborn/survivalgames/ControlType.java
+++ src/main/java/com/thenexusreborn/survivalgames/ControlType.java
@@ -3,6 +3,6 @@ package com.thenexusreborn.survivalgames;
 public enum ControlType {
     MANUAL, //No timers or progression logic is used. Progress triggered manually
     STEP, //Allows use of /sg game next to step forward in phases and steps
-    SEMI_AUTO, //Timers run, but do not trigger next steps
+    SEMI_AUTO, //Timers run, but does not trigger next steps
     AUTO //Fully auto, timers and progression logic
 }
