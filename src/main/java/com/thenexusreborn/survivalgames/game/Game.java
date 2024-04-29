ade2a6c9d805220c82c837ac76f6912189584a2a
Firestar311 <slaterandrew0398@gmail.com>
2024-04-29 14:06:57 -0600

    Fixed some time formats in the action bar

--- src/main/java/com/thenexusreborn/survivalgames/game/Game.java
+++ src/main/java/com/thenexusreborn/survivalgames/game/Game.java
@@ -65,7 +65,7 @@ import static com.thenexusreborn.survivalgames.game.GameState.*;
 @SuppressWarnings({"unused"})
 public class Game {
     private static final SurvivalGames plugin = SurvivalGames.getPlugin(SurvivalGames.class);
-    public static final TimeFormat SHORT_TIME_FORMAT = new TimeFormat("%*00h%%*#0m%%*#0s%");
+    public static final TimeFormat SHORT_TIME_FORMAT = new TimeFormat("%*#0h%%*#0m%%*#0s%");
     public static final TimeFormat TIME_FORMAT = new TimeFormat("%*00h%%#0m%%00s%");
     public static final TimeFormat LONG_TIME_FORMAT = new TimeFormat("%*00h%%00m%%00s%");
 
--- src/main/java/com/thenexusreborn/survivalgames/game/GameActionBar.java
+++ src/main/java/com/thenexusreborn/survivalgames/game/GameActionBar.java
@@ -50,7 +50,7 @@ public class GameActionBar implements IActionBar {
         }
 
         if (game.getState() == GameState.WARMUP) {
-            return "&fGAME BEGINS IN &c&l" + TimeUnit.SECONDS.fromMillis(game.getTimer().getTime()) + " SECONDS&f...";
+            return "&fGAME BEGINS IN &c&l" + (TimeUnit.MILLISECONDS.toSeconds(game.getTimer().getTime()) + 1) + " SECONDS&f...";
         }
 
         if (game.getState() == GameState.WARMUP_DONE) {
@@ -114,7 +114,7 @@ public class GameActionBar implements IActionBar {
             if (game.getTimer() == null) {
                 return "&aArchiving game...";
             }
-            return "&d&lNEXUS&7 - &eNext game starting... &7(" + game.getTimer().getTime() + "s)";
+            return "&d&lNEXUS&7 - &eNext game starting... &7(" + Game.SHORT_TIME_FORMAT.format(game.getTimer().getTime()) + ")";
         }
 
         return "";
