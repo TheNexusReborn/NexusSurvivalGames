9fbd9e37fd8f066bfc009da4291fd962f9580ede
Firestar311 <slaterandrew0398@gmail.com>
2025-02-09 21:25:30 -0600

    Made updating menus possible

--- src/main/java/com/thenexusreborn/survivalgames/menu/PlayerMenu.java
+++ src/main/java/com/thenexusreborn/survivalgames/menu/PlayerMenu.java
@@ -7,6 +7,7 @@ import com.stardevllc.starui.GuiManager;
 import com.stardevllc.starui.element.Element;
 import com.stardevllc.starui.element.button.Button;
 import com.stardevllc.starui.gui.InventoryGUI;
+import com.stardevllc.starui.gui.UpdatingGUI;
 import com.thenexusreborn.nexuscore.util.MsgType;
 import com.thenexusreborn.survivalgames.SGPlayer;
 import com.thenexusreborn.survivalgames.SurvivalGames;
@@ -19,10 +20,19 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class PlayerMenu extends InventoryGUI {
-    public PlayerMenu(SurvivalGames plugin, GamePlayer player) {
-        super(1, "&lMenu " + player.getName());
+public class PlayerMenu extends InventoryGUI implements UpdatingGUI {
 
+    private SurvivalGames plugin;
+    private GamePlayer player;
+
+    public PlayerMenu(SurvivalGames plugin, GamePlayer player) {
+        super(1, "&lMenu " + player.getName(), player.getUniqueId());
+        this.plugin = plugin;
+        this.player = player;
+    }
+
+    @Override
+    public void createItems() {
         SGPlayer sgPlayer = plugin.getPlayerRegistry().get(player.getUniqueId());
 
         Button teleportButton = new Button().iconCreator(p -> ItemBuilder.of(XMaterial.ENDER_PEARL).displayName("&e&lTeleport").build())
@@ -30,47 +40,50 @@ public class PlayerMenu extends InventoryGUI {
                     e.getWhoClicked().teleport(Bukkit.getPlayer(player.getUniqueId()).getLocation());
                     e.getWhoClicked().sendMessage(StarColors.color(MsgType.INFO + "Teleported to " + player.getColoredName() + "&e."));
                 });
-
-        List<String> vitalsLore = player.getMenuVitals().stream().map(line -> "  " + line).collect(Collectors.toCollection(LinkedList::new));
-        Element vitalsElement = new Element().iconCreator(p -> ItemBuilder.of(XMaterial.GLASS_BOTTLE).displayName("&e&lVitals").setLore(vitalsLore).build());
-
-        Button viewInventoryButton = new Button().iconCreator(p -> ItemBuilder.of(XMaterial.ENDER_CHEST).displayName("Inventory").addLoreLine("&c&lNOT YET IMPLEMENTED").build())
-                .consumer(e -> e.getWhoClicked().sendMessage(StarColors.color(MsgType.WARN + "This feature is not yet implemented.")));
-
-        Button sponsorButton = new Button().iconCreator(p -> ItemBuilder.of(XMaterial.CHEST).displayName("&e&lSponsor").build())
-                .consumer(e -> {
-                    GuiManager manager = plugin.getServer().getServicesManager().getRegistration(GuiManager.class).getProvider();
-                    SGPlayer actor = plugin.getPlayerRegistry().get(e.getWhoClicked().getUniqueId());
-                    if (actor.getGame() == null) {
-                        e.getWhoClicked().sendMessage(MsgType.WARN.format("You are not in a game."));
-                        return;
-                    }
-                    
-                    GamePlayer gp = actor.getGamePlayer();
-                    if (!sgPlayer.getGame().getSettings().isAllowSponsoring()) {
-                        gp.sendMessage(MsgType.WARN + "Sponsoring is disabled for this game.");
-                        return;
-                    }
-
-                    if (!player.getToggleValue("allowsponsors")) {
-                        gp.sendMessage(MsgType.WARN + "That player has sponsors disabled.");
-                        return;
-                    }
-
-                    manager.openGUI(new SponsorMenu(plugin, gp, player), (Player) e.getWhoClicked());
-                });
-        List<String> statsLore = player.getMenuStats().stream().map(line -> "  " + line).collect(Collectors.toCollection(LinkedList::new));
-        Element statsElement = new Element().iconCreator(p -> ItemBuilder.of(XMaterial.PAPER).displayName("&e&lStats").setLore(statsLore).build());
-
-        Element typeElement = new Element().iconCreator(p -> ItemBuilder.of(XMaterial.PLAYER_HEAD).displayName(player.getTeam().getColor() + "&l" + player.getTeam().name().substring(0, player.getTeam().name().length() - 1)).build());
-
         addElement(teleportButton);
-        addElement(vitalsElement);
-        addElement(viewInventoryButton);
+
+        if (player.getTeam() != GameTeam.SPECTATORS) {
+            List<String> vitalsLore = player.getMenuVitals().stream().map(line -> "  " + line).collect(Collectors.toCollection(LinkedList::new));
+            Element vitalsElement = new Element().iconCreator(p -> ItemBuilder.of(XMaterial.GLASS_BOTTLE).displayName("&e&lVitals").setLore(vitalsLore).build());
+            addElement(vitalsElement);
+
+            Button viewInventoryButton = new Button().iconCreator(p -> ItemBuilder.of(XMaterial.ENDER_CHEST).displayName("Inventory").addLoreLine("&c&lNOT YET IMPLEMENTED").build())
+                    .consumer(e -> e.getWhoClicked().sendMessage(StarColors.color(MsgType.WARN + "This feature is not yet implemented.")));
+            addElement(viewInventoryButton);
+        }
+
         if (sgPlayer.getGame().getSettings().isAllowSponsoring() && player.getToggleValue("allowsponsors") && player.getTeam() == GameTeam.TRIBUTES) {
+            Button sponsorButton = new Button().iconCreator(p -> ItemBuilder.of(XMaterial.CHEST).displayName("&e&lSponsor").build())
+                    .consumer(e -> {
+                        GuiManager manager = plugin.getServer().getServicesManager().getRegistration(GuiManager.class).getProvider();
+                        SGPlayer actor = plugin.getPlayerRegistry().get(e.getWhoClicked().getUniqueId());
+                        if (actor.getGame() == null) {
+                            e.getWhoClicked().sendMessage(MsgType.WARN.format("You are not in a game."));
+                            return;
+                        }
+
+                        GamePlayer gp = actor.getGamePlayer();
+                        if (!sgPlayer.getGame().getSettings().isAllowSponsoring()) {
+                            gp.sendMessage(MsgType.WARN + "Sponsoring is disabled for this game.");
+                            return;
+                        }
+
+                        if (!player.getToggleValue("allowsponsors")) {
+                            gp.sendMessage(MsgType.WARN + "That player has sponsors disabled.");
+                            return;
+                        }
+
+                        manager.openGUI(new SponsorMenu(plugin, gp, player), (Player) e.getWhoClicked());
+                    });
+
             addElement(sponsorButton);
         }
+
+        List<String> statsLore = player.getMenuStats().stream().map(line -> "  " + line).collect(Collectors.toCollection(LinkedList::new));
+        Element statsElement = new Element().iconCreator(p -> ItemBuilder.of(XMaterial.PAPER).displayName("&e&lStats").setLore(statsLore).build());
         addElement(statsElement);
+
+        Element typeElement = new Element().iconCreator(p -> ItemBuilder.of(XMaterial.PLAYER_HEAD).displayName(player.getTeam().getColor() + "&l" + player.getTeam().name().substring(0, player.getTeam().name().length() - 1)).build());
         addElement(typeElement);
     }
 }
