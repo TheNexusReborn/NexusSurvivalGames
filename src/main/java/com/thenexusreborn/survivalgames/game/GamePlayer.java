50ec937163126a932483e292d9efcd73ada1112b
Firestar311 <slaterandrew0398@gmail.com>
2025-02-11 19:15:08 -0600

    Added a getGame method to the GamePlayer class

--- src/main/java/com/thenexusreborn/survivalgames/game/GamePlayer.java
+++ src/main/java/com/thenexusreborn/survivalgames/game/GamePlayer.java
@@ -55,6 +55,10 @@ public class GamePlayer {
         this.stats = stats;
     }
 
+    public Game getGame() {
+        return game;
+    }
+
     public SGPlayerStats getStats() {
         return stats;
     }
