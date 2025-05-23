a129b9447bbd0f661313470ab345236683ce5d94
Firestar311 <slaterandrew0398@gmail.com>
2025-05-23 16:06:38 -0500

    Some loot rebalances

--- src/main/java/com/thenexusreborn/survivalgames/loot/tables/TierTwoLootTable.java
+++ src/main/java/com/thenexusreborn/survivalgames/loot/tables/TierTwoLootTable.java
@@ -14,7 +14,7 @@ public class TierTwoLootTable extends SGLootTable {
         addItems(40, Items.PORKCHOP, Items.STEAK, Items.GRILLED_CHICKEN, Items.PUMPKIN_PIE, Items.VILE_CREATURE, Items.BAKED_POTATO);
         addItems(50, Items.RAW_PORKCHOP, Items.RAW_BEEF, Items.RAW_FISH);
         addItems(45, Items.APPLE, Items.CAKE, Items.GOLDEN_CARROT);
-        addItems(15, Items.GOLDEN_MUNCHIE);
+        addItems(8, Items.GOLDEN_MUNCHIE);
 
         addItems(35, Items.THE_CROWN, Items.GOLD_CHESTPLATE, Items.GOLD_LEGGINGS, Items.GOLD_BOOTS);
         addItems(25, Items.LINGERIE_HELMET, Items.LINGERIE_CHESTPLATE, Items.LINGERIE_LEGGINGS, Items.LINGERIE_BOOTS);
@@ -24,14 +24,14 @@ public class TierTwoLootTable extends SGLootTable {
         addItems(20, Items.STONE_AXE, Items.BOW);
         addItems(15, Items.STONE_SWORD);
 
-        addItems(30, Items.EGG_OF_DOOM, Items.SLOWBALL, Items.XP_BOTTLE, Items.ENDER_PEARL);
+        addItems(30, Items.EGG_OF_DOOM, Items.SLOWBALL, Items.XP_BOTTLE);
         addItems(15, Items.COBWEB, Items.TNT);
 
         addItems(20, Items.ARROW.setAmount(5));
-        addItems(15, Items.PLAYER_TRACKER, Items.FLINT_AND_STEEL, Items.FISHING_ROD);
+        addItems(15, Items.PLAYER_TRACKER, Items.FLINT_AND_STEEL, Items.FISHING_ROD, Items.ENDER_PEARL);
 
         addItems(25, Items.FEATHER, Items.FLINT, Items.STICK);
         addItems(20, Items.GOLD_INGOT, Items.IRON_INGOT);
-        addItems(5, Items.DIAMOND);
+        addItems(10, Items.DIAMOND);
     }
 }
