471969fa98fada0e053e1f527219a91b91d5dbc6
Firestar311 <slaterandrew0398@gmail.com>
2025-05-27 17:54:15 -0500

    Fixed a Voting Weight formatting issue

--- src/main/java/com/thenexusreborn/survivalgames/lobby/Lobby.java
+++ src/main/java/com/thenexusreborn/survivalgames/lobby/Lobby.java
@@ -964,7 +964,7 @@ public class Lobby implements Controllable, IHasState {
                 } else {
                     player.sendMessage("&6&l>> &eYou voted for&8: &b" + sgMap.getName() + " &7&oby &3" + creators + "&e.");
                 }
-                player.sendMessage("&6&l>> Voting Weight&8&l: &b" + VOTE_WEIGHTS.get(nexusPlayer.getEffectiveRank()) + " Vote(s)&e.");
+                player.sendMessage("&6&l>> &eVoting Weight&8&l: &b" + VOTE_WEIGHTS.get(nexusPlayer.getEffectiveRank()) + " Vote(s)&e.");
                 player.playSound(Sound.NOTE_PLING);
 
                 player.setMapVote(entry.getKey());
