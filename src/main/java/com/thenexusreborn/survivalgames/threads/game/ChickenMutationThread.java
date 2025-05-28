49677a4ddb471c1ca706f7b66d513a941a599edf
Firestar311 <slaterandrew0398@gmail.com>
2025-05-28 16:10:40 -0500

    Added some debug information to the chicken mutation thread to diagnose issues with the egg launcher

--- src/main/java/com/thenexusreborn/survivalgames/threads/game/ChickenMutationThread.java
+++ src/main/java/com/thenexusreborn/survivalgames/threads/game/ChickenMutationThread.java
@@ -47,24 +47,59 @@ public class ChickenMutationThread extends StarThread<SurvivalGames> {
             }
         
             if (gamePlayer.getMutation() instanceof ChickenMutation chickenMutation) {
+                game.logDebug("Handling Chicken Mutation Update for " + gamePlayer.getName());
                 if (this.eggGain == 0) {
                     chickenMutation.incrementAmmunition();
+                    game.logDebug("  Incremented Ammo");
                 }
                 
+                game.logDebug("  Ammo: " + chickenMutation.getAmmunition());
+                game.logDebug("  Chute Active: " + chickenMutation.isChuteActive());
+                game.logDebug("  Chute Cooldown Left: " + chickenMutation.getParachuteCooldownTimer().getTime());
+                game.logDebug("  Launch Left: " + chickenMutation.getLaunchCooldownTimer().getTime());
+                game.logDebug("  Last Launch: " + chickenMutation.getLastEggLaunch());
+                game.logDebug("  Current Time: " + System.currentTimeMillis());
+                
                 ItemStack hand = sgPlayer.getItemInHand();
-                if (sgPlayer.isBlocking() && hand != null && hand.getType() == Material.WOOD_SWORD && chickenMutation.getAmmunition() > 0) {
-                    if (chickenMutation.getLastEggLaunch() > 0) {
-                        if (chickenMutation.getLastEggLaunch() + game.getSettings().getChickenEggLaunchCooldown() >= System.currentTimeMillis()) {
-                            sgPlayer.launchProjectile(Egg.class);
-                            chickenMutation.decrementAmmunition();
-                            chickenMutation.setLastEggLaunch(System.currentTimeMillis());
+                if (sgPlayer.isBlocking()) {
+                    game.logDebug("  Player is Blocking");
+                    if (hand != null) {
+                        game.logDebug("  Item in Hand is Not Null");
+                        if (hand.getType() == Material.WOOD_SWORD) {
+                            game.logDebug("  Item in Hand is Wood Sword");
+                            if (chickenMutation.getAmmunition() > 0) {
+                                game.logDebug("  Ammo Amount is above 0");
+                                if (chickenMutation.getLastEggLaunch() > 0) {
+                                    if (chickenMutation.getLastEggLaunch() + game.getSettings().getChickenEggLaunchCooldown() >= System.currentTimeMillis()) {
+                                        game.logDebug("  Egg Launch is off cooldown");
+                                        sgPlayer.launchProjectile(Egg.class);
+                                        chickenMutation.decrementAmmunition();
+                                        chickenMutation.setLastEggLaunch(System.currentTimeMillis());
+                                    }
+                                } else {
+                                    game.logDebug("  Egg Launcher has not been used");
+                                    sgPlayer.launchProjectile(Egg.class);
+                                    chickenMutation.decrementAmmunition();
+                                    chickenMutation.setLastEggLaunch(System.currentTimeMillis());
+                                }
+                            }
                         }
-                    } else {
-                        sgPlayer.launchProjectile(Egg.class);
-                        chickenMutation.decrementAmmunition();
-                        chickenMutation.setLastEggLaunch(System.currentTimeMillis());
                     }
                 }
+                
+//                if (sgPlayer.isBlocking() && hand != null && hand.getType() == Material.WOOD_SWORD && chickenMutation.getAmmunition() > 0) {
+//                    if (chickenMutation.getLastEggLaunch() > 0) {
+//                        if (chickenMutation.getLastEggLaunch() + game.getSettings().getChickenEggLaunchCooldown() >= System.currentTimeMillis()) {
+//                            sgPlayer.launchProjectile(Egg.class);
+//                            chickenMutation.decrementAmmunition();
+//                            chickenMutation.setLastEggLaunch(System.currentTimeMillis());
+//                        }
+//                    } else {
+//                        sgPlayer.launchProjectile(Egg.class);
+//                        chickenMutation.decrementAmmunition();
+//                        chickenMutation.setLastEggLaunch(System.currentTimeMillis());
+//                    }
+//                }
                 
                 sgPlayer.setLevel(chickenMutation.getAmmunition());
                 
