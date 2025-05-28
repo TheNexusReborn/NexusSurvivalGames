4cb06ae4df6303ec7ec37137e03a278f3a9134df
Firestar311 <slaterandrew0398@gmail.com>
2025-05-28 14:18:34 -0500

    Added players to the sg api

--- src/main/java/com/thenexusreborn/survivalgames/threads/ServerStatusThread.java
+++ src/main/java/com/thenexusreborn/survivalgames/threads/ServerStatusThread.java
@@ -1,14 +1,13 @@
 package com.thenexusreborn.survivalgames.threads;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
+import com.stardevllc.helper.StringHelper;
 import com.stardevllc.starcore.utils.StarThread;
 import com.stardevllc.time.TimeUnit;
 import com.thenexusreborn.survivalgames.SurvivalGames;
-import com.thenexusreborn.survivalgames.game.Game;
-import com.thenexusreborn.survivalgames.game.GameTeam;
+import com.thenexusreborn.survivalgames.game.*;
 import com.thenexusreborn.survivalgames.lobby.Lobby;
+import com.thenexusreborn.survivalgames.lobby.LobbyPlayer;
 import com.thenexusreborn.survivalgames.server.SGVirtualServer;
 
 public class ServerStatusThread extends StarThread<SurvivalGames> {
@@ -33,13 +32,24 @@ public class ServerStatusThread extends StarThread<SurvivalGames> {
                 playersObject.addProperty("tributes", game.getTeamCount(GameTeam.TRIBUTES));
                 playersObject.addProperty("spectators", game.getTeamCount(GameTeam.SPECTATORS));
                 playersObject.addProperty("mutations", game.getTeamCount(GameTeam.MUTATIONS));
-                stateObject.add("players", playersObject);
+                stateObject.add("teamCounts", playersObject);
                 stateObject.addProperty("mode", game.getMode().name());
                 stateObject.addProperty("state", game.getState().name()); //Replace with a JsonObject when new state system is fully implemented
                 JsonObject timeObject = new JsonObject();
                 timeObject.addProperty("main", game.getTimer() != null ? game.getTimer().getTime() : 0L);
                 timeObject.addProperty("grace", game.getGraceperiodTimer() != null ? game.getGraceperiodTimer().getTime() : 0);
                 stateObject.add("time", timeObject);
+                
+                JsonArray playersArray = new JsonArray();
+                for (GamePlayer player : game.getPlayers().values()) {
+                    if (!player.getToggleValue("vanish") && !player.getToggleValue("incognito")) {
+                        JsonObject playerObject = new JsonObject();
+                        playerObject.addProperty("uuid", player.getUniqueId().toString());
+                        playerObject.addProperty("name", player.getName());
+                        playerObject.addProperty("team", StringHelper.titlize(player.getTeam().name()));
+                        playersArray.add(playerObject);
+                    }
+                }
             } else if (lobby != null) {
                 stateObject.addProperty("type", "lobby");
                 stateObject.addProperty("map", lobby.getGameMap() != null ? lobby.getGameMap().getName() : "Voting");
@@ -49,7 +59,19 @@ public class ServerStatusThread extends StarThread<SurvivalGames> {
                 JsonObject playersObject = new JsonObject();
                 playersObject.addProperty("playing", lobby.getPlayingCount());
                 playersObject.addProperty("spectating", lobby.getSpectatingPlayers().size());
-                stateObject.add("players", playersObject);
+                stateObject.add("playerCounts", playersObject);
+                
+                JsonArray playersArray = new JsonArray();
+                for (LobbyPlayer player : lobby.getPlayers()) {
+                    if (!player.getToggleValue("vanish") && !player.getToggleValue("incognito")) {
+                        JsonObject playerObject = new JsonObject();
+                        playerObject.addProperty("uuid", player.getUniqueId().toString());
+                        playerObject.addProperty("name", player.getName());
+                        playersArray.add(playerObject);
+                    }
+                }
+                
+                stateObject.add("players", playersArray);
             }
             server.setState(stateObject.toString());
         }
