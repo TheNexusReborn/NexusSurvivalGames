ef44becfbd908edbeb0f84f58428f3903d0f8723
Firestar311 <slaterandrew0398@gmail.com>
2025-05-28 00:36:58 -0500

    Added some checks for the server select event in SG to prevent errors if the nexusplayer is null

--- src/main/java/com/thenexusreborn/survivalgames/hooks/NexusHubHook.java
+++ src/main/java/com/thenexusreborn/survivalgames/hooks/NexusHubHook.java
@@ -2,11 +2,16 @@ package com.thenexusreborn.survivalgames.hooks;
 
 import com.thenexusreborn.hub.NexusHub;
 import com.thenexusreborn.hub.api.ServerSelectEvent;
+import com.thenexusreborn.nexuscore.util.MsgType;
 import com.thenexusreborn.survivalgames.SurvivalGames;
 import com.thenexusreborn.survivalgames.server.SGVirtualServer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
+
+import java.util.UUID;
 
 public class NexusHubHook implements Listener {
     private final SurvivalGames survivalGames;
@@ -20,6 +25,19 @@ public class NexusHubHook implements Listener {
     @EventHandler
     public void onServerSelect(ServerSelectEvent e) {
         String serverName = e.getServerName();
+        
+        if (e.getNexusPlayer() == null) {
+            survivalGames.getLogger().severe("NexusPlayer in a ServerSelectEvent was null.");
+            UUID uuid = e.getUuid();
+            Player player = Bukkit.getPlayer(uuid);
+            if (player != null) {
+                MsgType.ERROR.send(player, "There was an error while processing your server selection. Please leave and rejoin.");
+                survivalGames.getLogger().severe("  The player was online: " + player.getName());
+            }
+            
+            return;
+        }
+        
         for (SGVirtualServer server : survivalGames.getServers()) {
             if (server.getName().equalsIgnoreCase(serverName)) {
                 server.join(e.getNexusPlayer());
