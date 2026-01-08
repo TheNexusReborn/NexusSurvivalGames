c43d6ba7e8a3438d368f33b6a10f48a1df6b6161
Firestar311 <slaterandrew0398@gmail.com>
2026-01-08 10:28:22 -0600

    Added unbreaking 10 to the gold sword for mutations

--- src/main/java/com/thenexusreborn/survivalgames/mutations/MutationItem.java
+++ src/main/java/com/thenexusreborn/survivalgames/mutations/MutationItem.java
@@ -4,10 +4,11 @@ import com.stardevllc.itembuilder.ItemBuilders;
 import com.stardevllc.itembuilder.common.ItemBuilder;
 import com.stardevllc.smaterial.SMaterial;
 import com.stardevllc.staritems.model.CustomItem;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
 public final class MutationItem {
-    public static final ItemBuilder UNBREAKABLE_GOLD_SWORD = ItemBuilders.of(SMaterial.GOLDEN_SWORD).displayName("&fUnbreakable Golden Sword").unbreakable(true);
+    public static final ItemBuilder UNBREAKABLE_GOLD_SWORD = ItemBuilders.of(SMaterial.GOLDEN_SWORD).displayName("&fGolden Sword").addEnchant(Enchantment.DURABILITY, 10).unbreakable(true);
     private final int slotOffset;
     private ItemBuilder itemBuilder;
     private CustomItem customItem;
